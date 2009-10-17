1b463d5ec394710586e0cf612116afa61531ffca
Dan Croak <dcroak@thoughtbot.com>
2009-10-17 14:35:47 -0400

    use mongo instead of sequel

--- shorty.rb
+++ shorty.rb
@@ -1,29 +1,37 @@
 require 'rubygems'
 require 'sinatra'
-require 'database'
+require 'mongo'
+
+include Mongo
+
+DB = Connection.new(ENV['DATABASE_URL'] || 'localhost').db('shorty')
+if ENV['DATABASE_USER'] && ENV['DATABASE_PASSWORD']
+  DB.authenticate(ENV['DATABASE_USER'], ENV['DATABASE_PASSWORD'])
+end
 
 get '/' do
   <<-HTML
-    <title>Shorty</title>
+    <title>URL shortener</title>
     <form action="/shorten" method="post">
       <input type="text" name="url" />
       <input type="submit" value="shorten" />
     </form>
+    <p>Written in Sinatra and MongoDB.</p>
   HTML
 end
 
 post '/shorten' do
-  reject_blank params[:url]
-  shorten      params[:url]
-  id = id_for  params[:url]
-  "<a href='/#{id}'>http://capeco.de/#{id}</a>"
+  reject_blank    params[:url]
+  shorten         params[:url]
+  slug = slug_for params[:url]
+  "<a href='/#{slug}'>http://#{Sinatra::Application.host}/#{slug}</a>"
 end
 
-get '/:id' do |id|
-  if url = url_for(id)
+get '/:slug' do |slug|
+  if url = url_for(slug)
     redirect(url)
   else
-    "Not Found"
+    halt(404)
   end
 end
 
@@ -33,14 +41,14 @@ helpers do
   end
 
   def shorten(url)
-    database[:urls].insert(:url => url) unless Url.find(:url => url)
+    DB['urls'].insert('url' => url, 'slug' => DB['urls'].count.to_s(36))
   end
 
-  def id_for(url)
-    database["select id from urls where url = ?", url].to_s(36)
+  def slug_for(url)
+    DB['urls'].find('url' => url).collect {|row| row['slug'] }.flatten
   end
 
-  def url_for(id)
-    database["select url from urls where id = ?", id.to_i(36)]
+  def url_for(slug)
+    DB['urls'].find('slug' => slug).collect {|row| row['url'] }.flatten
   end
 end
