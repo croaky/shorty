4d04ef939b62eebf0d00b12a486413a511a0faae
Dan Croak <dcroak@thoughtbot.com>
2009-10-18 15:21:21 -0400

    added ideal acceptance test

---
+++ features/shorten_url.rb
@@ -0,0 +1,22 @@
+Feature 'Shorten URL' do
+  Given 'I am on the homepage' do
+    get '/'
+  end
+
+  When 'I submit http://dancroak.com' do
+    fill_in      'url', :with => 'http://dancroak.com'
+    click_button 'shorten'
+  end
+
+  Then 'I should see a short link' do
+    assert_have_selector 'a#short'
+  end
+
+  When 'I follow the short link' do
+    click_link 'short'
+  end
+
+  Then 'I should be on http://dancroak.com' do
+    assert_equal 'http://dancroak.com', current_url
+  end
+end
