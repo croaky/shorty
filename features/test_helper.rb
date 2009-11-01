9c6bacec5ad75c98dc9b024c4775f81f6018865c
Dan Lawless <lawless@dan-lawlesss-macbook.local>
2009-10-31 20:09:16 -0400

    using const_set instead of eval()

--- features/test_helper.rb
+++ features/test_helper.rb
@@ -8,32 +8,40 @@ Webrat.configure do |config|
   config.mode = :rack
 end
 
-include Rack::Test::Methods
-include Webrat::Methods
-include Webrat::Matchers
-include Webrat::HaveTagMatcher
+module Teleplay
+  include Rack::Test::Methods
+  include Webrat::Methods
+  include Webrat::Matchers
+  include Webrat::HaveTagMatcher
 
-def app
-  Sinatra::Application
+  def app
+    Sinatra::Application
+  end
+
+  Statement = Struct.new(:type, :name, :block)
+
+  def Given(name, &block); Statement.new("Given", name, block); end
+  def When(name, &block);  Statement.new("When",  name, block); end
+  def Then(name, &block);  Statement.new("Then",  name, block); end
+  def And(name, &block);   Statement.new("And",   name, block); end
 end
 
-Statement = Struct.new(:type, :name, :block)
-
-def Given(name, &block); Statement.new("Given", name, block); end
-def When(name, &block);  Statement.new("When",  name, block); end
-def Then(name, &block);  Statement.new("Then",  name, block); end
-def And(name, &block);   Statement.new("And",   name, block); end
+class Test::Unit::TestCase
+  include Teleplay
+end
 
 def Feature(name, &block)
-  block.call
-  eval(
-    "class #{class_name(name)} < Test::Unit::TestCase
-       def default_test
-         statements = []
-         ObjectSpace.each_object(Statement) { |s| statements << s }
-         statements.reverse.each { |s| s.block.call }
-       end
-     end")
+  test_class = Module.const_set(class_name(name), Class.new(Test::Unit::TestCase))
+
+  test_class.class_eval do
+    def default_test
+      statements = []
+      ObjectSpace.each_object(Statement) { |s| statements << s }
+      statements.reverse.each { |s| s.block.call }
+    end
+  end
+
+  test_class.class_eval(&block)
 end
 
 def class_name(name)
