ed75ee3ea1e7555c6814c8863bcb83770856ad26
Dan Lawless <lawless@dan-lawlesss-macbook.local>
2009-11-01 14:08:31 -0500

    module-izing

--- features/test_helper.rb
+++ features/test_helper.rb
@@ -8,22 +8,28 @@ Webrat.configure do |config|
   config.mode = :rack
 end
 
+class Statement < Struct.new(:type, :name, :block); end
+
 module Teleplay
-  include Rack::Test::Methods
-  include Webrat::Methods
-  include Webrat::Matchers
-  include Webrat::HaveTagMatcher
+  def self.included(model)
+    model.extend(ClassMethods)
+
+    model.send(:include, Rack::Test::Methods)
+    model.send(:include, Webrat::Methods)
+    model.send(:include, Webrat::Matchers)
+    model.send(:include, Webrat::HaveTagMatcher)
+  end
+
+  module ClassMethods
+    def Given(name, &block); Statement.new("Given", name, block); end
+    def When(name, &block);  Statement.new("When",  name, block); end
+    def Then(name, &block);  Statement.new("Then",  name, block); end
+    def And(name, &block);   Statement.new("And",   name, block); end
+  end
 
   def app
     Sinatra::Application
   end
-
-  Statement = Struct.new(:type, :name, :block)
-
-  def Given(name, &block); Statement.new("Given", name, block); end
-  def When(name, &block);  Statement.new("When",  name, block); end
-  def Then(name, &block);  Statement.new("Then",  name, block); end
-  def And(name, &block);   Statement.new("And",   name, block); end
 end
 
 class Test::Unit::TestCase
